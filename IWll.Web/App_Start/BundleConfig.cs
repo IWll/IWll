3a723fcbdf220f96e5e1edd160cba91e0f83b1dd
yjzzcao <yjzzcao@gmail.com>
2015-03-18 17:05:42 +0800

    start IWll

---
+++ IWll.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Optimization;
+
+namespace IWll.Web
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/site.css"));
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
