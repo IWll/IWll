3c8f61223f7e056f7a41f20a8dde152b382832e5
yjzzcao <yjzzcao@gmail.com>
2015-03-21 17:09:56 +0800

    Manager API

---
+++ IWll.Models/Manager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IWll.Models
+{
+    public class Manager
+    {
+        public int ManagerID { get; set; }
+        public string ManagerName { get; set; }
+        public string ManagerPassword { get; set; }
+    }
+}
---
+++ IWll.Web/Controllers/ManagerController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using IWll.Models;
+using IWll.IBLL;
+using IWll.BLL;
+
+namespace IWll.Web.Controllers
+{
+    public class ManagerController : ApiController
+    {
+        private InterfaceManagerService managerService;
+        public ManagerController() { managerService = new ManagerService(); }
+        //public IEnumerable<Manager> GetAllProducts()
+        //{
+        //    return products;
+        //}
+        //public IEnumerable<Manager> GetAllManagers()
+        //{
+
+        //    return managers;
+        //}
+        //public bool GetManager()
+        //{
+        //    return true;
+        //}
+
+        public bool GetManagerLogin(Manager manager)
+        {
+            return managerService.Login(manager.ManagerName, manager.ManagerPassword);
+        }
+        
+    }
+}
